1b3674e27976f72c33a971d564f2aafa821e7d59
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-03-31 14:51:50 +0200

    working with linux

--- main.js
+++ main.js
@@ -1,34 +1,126 @@
 const child_process=require("child_process");
-let tracks=[];
-let playIndex=0;
+const players={};
 const trackTemplate={
 	id: null,
 	name: null,
 	src: null,
 };
+const platform=process.platform==="win32"?"windows":process.platform;
 
-function addTrack(track){
+function addTrack(id,object){
+	const player=players[id];
 	const track={
 		...trackTemplate,
-		...track,
+		...object,
+		id: Date.now(),
 	};
-	if(track.id) tracks.push(track);
+	if(!track.id) return;
+
+	player.setPlayerKey("tracks",[
+		...player.getPlayerKey("tracks"),
+		track,
+	]);
 }
-function addTracks(tracks){
-	for(const track of tracks){
-		addTrack(track);
+function play(id){
+	const player=players[id];
+	console.log("this is coming later... o_o");
+	if(player.getPlayerKey("isPlaying")){
+		return false;
+	}
+	player.setPlayerKey("isPlaying",true);
+	const src=player.getPlayerKey("tracks")[player.getPlayerKey("trackIndex")];
+	player.setPlayerKey("srcPlaying",src);
+	if(platform==="windows"){
+		throw new Error("Windows is not allowed ....");
+	}
+	else{
+		const tracks=player.getPlayerKey("tracks");
+		const trackIndex=player.getPlayerKey("trackIndex");
+		const track=tracks[trackIndex];
+
+		player.setPlayerKey("playerProcess",child_process.spawn("/usr/bin/mplayer",[track.src]));
+		player.getPlayerKey("playerProcess").on("exit",code=>{
+			player.setPlayerKey("playerProcess",undefined);
+			if(player.getPlayerKey("isPlaying")){
+				player.setPlayerKey("isPlaying",false);
+				player.nextTrack();
+			}
+		});
 	}
 }
-function removeTrack(id){
-	tracks=tracks.filter(item=>item.id!==id);
+function nextTrack(id){
+	const player=players[id];
+	if(player.getPlayerKey("isPlaying")){
+		player.setPlayerKey("isPlaying",false);
+		player.getPlayerKey("playerProcess").kill();
+		player.setPlayerKey("playerProcess",undefined);
+	}
+	const tracks=player.getPlayerKey("tracks");
+	let trackIndex=player.getPlayerKey("trackIndex");
+	const tracksLength=tracks.length-1;
+
+	trackIndex+=1;
+
+	if(trackIndex>tracksLength){
+		trackIndex=0;
+	}
+
+	player.setPlayerKey("trackIndex",trackIndex);
+	player.play();
 }
-function play(){
-	console.log("this is coming later... o_o");
+function pause(id){
+	const player=players[id];
+	if(player.getPlayerKey("isPlaying")){
+		player.setPlayerKey("isPlaying",false);
+		player.getPlayerKey("playerProcess").kill();
+		player.setPlayerKey("playerProcess",undefined);
+		player.setPlayerKey("srcPlaying",null);
+	}
+	
+}
+function stop(id){
+	const player=players[id];
+	if(player.getPlayerKey("isPlaying")){
+		player.pause();
+	}
+	player.setPlayerKey("trackIndex",0);
+}
+function getPlayer(id){
+	return players[id];
+}
+function getPlayerKey(id,key){
+	return players[id][key];
+}
+function setPlayer(id,object){
+	players[id]=object;
+}
+function setPlayerKey(id,key,value){
+	players[id][key]=value;
+}
+function createPlayer(){
+	const id=Date.now();
+	const playerCommands={
+		addTrack: track=> addTrack(id,track),
+		getPlayer:()=> getPlayer(id),
+		getPlayerKey: key=> getPlayerKey(id,key),
+		nextTrack:()=> nextTrack(id),
+		pause:()=> pause(id),
+		play:()=> play(id),
+		stop:()=> stop(id),
+	};
+	players[id]={
+		...playerCommands,
+		isPlaying: false,
+		srcPlaying: null,
+		trackIndex: 0,
+		tracks:[],
+		setPlayer:()=> setPlayer(id),
+		setPlayerKey: (key,value)=> setPlayerKey(id,key,value),
+
+	};
+	return playerCommands;
 }
 
 module.exports={
-	addTrack,
-	addTracks,
-	removeTrack,
-	play,
+	createPlayer,
 };
